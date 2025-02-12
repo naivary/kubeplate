765452b73d4ab6eadc3fc2a9aeb5aed9f4e73591
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2025-02-12 17:35:49 +0100

    getter is getting there (get it?)

--- plugin/get.go
+++ plugin/get.go
@@ -3,11 +3,12 @@ package plugin
 import (
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/hashicorp/go-getter"
 )
 
-func Get(url string, k Kind) (string, error) {
+func Get(url string) (string, error) {
 	// TODO: check if URL already exists localy or not
 	home, err := os.UserHomeDir()
 	if err != nil {
@@ -21,12 +22,15 @@ func Get(url string, k Kind) (string, error) {
 		c.Pwd = pwd
 		return nil
 	}
-	dst := filepath.Join(home, ".kubeplate", k.String())
+	dst := filepath.Join(home, ".kubeplate")
 	err = getter.GetAny(dst, url, opt)
 	return dst, err
 }
 
 func detectDir(url string) (string, error) {
+	// TODO: Bring every possible getter path to a http(s):// path 
+	// just to use url.Parse and get the url.Path to use as a directory
+	// structure
 	pwd, err := os.Getwd()
 	if err != nil {
 		return "", err
@@ -36,4 +40,19 @@ func detectDir(url string) (string, error) {
 		return "", err
 	}
 	return detected, nil
+}
+
+func cutPrefix(url string) (string, bool) {
+	switch {
+	case strings.HasPrefix(url, "https://") || strings.HasPrefix(url, "http://"):
+		return url, true
+	case strings.HasPrefix(url, "file://"):
+		return strings.CutPrefix(url, "file://")
+	case strings.HasPrefix(url, "git://"):
+		return strings.CutPrefix(url, "git://")
+	case strings.HasPrefix(url, "s3://"):
+		return strings.CutPrefix(url, "file://")
+	default:
+		return "UKNOWN", false
+	}
 }
--- plugin/get_test.go
+++ plugin/get_test.go
@@ -8,24 +8,21 @@ func TestGet(t *testing.T) {
 	tests := []struct {
 		name string
 		url  string
-		kind Kind
 		err  bool
 	}{
 		{
 			name: "plugin:local file",
 			url:  "file::../examples/plugin/inputer/json",
-			kind: Inputer,
 		},
 		{
 			name: "plugin:git repo",
-			url:  "http://github.com/hashicorp/go-getter",
-			kind: Inputer,
+			url:  "git::http://github.com/hashicorp/go-getter.git?ref=v1.7.8",
 		},
 	}
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			path, err := Get(tc.url, tc.kind)
+			path, err := Get(tc.url)
 			if err != nil && !tc.err {
 				t.Error(err)
 			}
@@ -42,8 +39,33 @@ func TestDetectDir(t *testing.T) {
 	}{
 		{
 			name:     "local file",
-			url:      "file::./foo/bar/inputer_binary",
+			url:      "file://foo/bar/inputer_binary",
 			expected: "bar",
+		},
+		{
+			name:     "local file without prefix",
+			url:      "./foo/bar/inputer_binary",
+			expected: "bar",
+		},
+		{
+			name:     "s3 bucket",
+			url:      "bucket.s3.amazonaws.com/foo",
+			expected: "bucket.s3.amazonaws.com/foo",
+		},
+		{
+			name:     "git repo ssh addresses",
+			url:      "git::ssh://git@example.com/foo/bar",
+			expected: "example.com/foo/bar",
+		},
+		{
+			name:     "git scp style",
+			url:      "git@github.com:naivary/Bachelorarbeit.git",
+			expected: "example.com/foo/bar",
+		},
+		{
+			name:     "git http",
+			url:      "git::http://github.com/mitchellh/vagrant.git",
+			expected: "github.com/mitchellh/vagrant",
 		},
 	}
 
